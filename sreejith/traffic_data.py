c4b68513370b7c8ca18b0d55f2454f31580761f1
Sreejith Ajithkumar Nair <161915448+Rockshore11@users.noreply.github.com>
2024-12-03 12:56:57 -0500

    imports updated

--- sreejith/traffic_data.py
+++ sreejith/traffic_data.py
@@ -1,14 +1,5 @@
-
-
-from utils import clear_screen
-import sys
-import os
-
-# Add the parent directory (Traffic-Insight) to the Python path
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(project_root)
-
 from Business.business_access_layer import BusinessAccessLayer
+from utils import clear_screen
 
 bus = BusinessAccessLayer()   
 
