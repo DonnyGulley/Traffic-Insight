883a4511ce9c31e9e0af2a896ff5ec683c8877df
Sreejith Ajithkumar Nair <161915448+Rockshore11@users.noreply.github.com>
2024-12-08 14:23:37 -0500

    added new table survey response and default admin

--- Data/Databases/Scripts/TrafficInsight_DB.sql
+++ Data/Databases/Scripts/TrafficInsight_DB.sql
@@ -32,6 +32,15 @@ CONSTRAINT [PK_Notifications] PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
+
+ALTER TABLE dbo.Notifications
+DROP CONSTRAINT FK_Notifications_User;
+
+ALTER TABLE dbo.Notifications
+ADD CONSTRAINT FK_Notifications_User
+FOREIGN KEY (UserId) REFERENCES dbo.[User](UserId)
+ON DELETE CASCADE;
+GO
 /****** Object:  Table [dbo].[Roles]    Script Date: 2024-11-21 5:26:08 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -131,3 +140,33 @@ REFERENCES [dbo].[Roles] ([RoleId])
 GO
 ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_Roles]
 GO
+
+
+
+ALTER TABLE [User]
+ADD SecurityQuestion VARCHAR(255), SecurityAnswer VARCHAR(255);
+GO
+-- Insert default admin user
+INSERT INTO [dbo].[User] 
+    ([username], [password], [email], [Consent], [RoleTypeId], [SecurityQuestion], [SecurityAnswer])
+VALUES 
+    ('admin', 
+    CONVERT(binary(256), 'password'), 
+    'admin@example.com', 
+    1,  -- Consent (1 for yes, 0 for no)
+    1,  -- RoleTypeId (1 could represent Admin, adjust as needed based on your RoleType table)
+    'adminkey', 
+    '1234');
+GO
+
+
+CREATE TABLE [dbo].[SurveyResponses] (
+    [SurveyResponseId] INT IDENTITY(1,1) PRIMARY KEY,  -- Auto-incrementing ID
+    [UserId] INT NOT NULL,                             -- Foreign key to the User table
+    [Question] VARCHAR(255) NOT NULL,                  -- Survey question
+    [Answer] VARCHAR(255) NOT NULL,                    -- User's answer
+    [DateSubmitted] DATETIME NOT NULL DEFAULT GETDATE(), -- Date and time of submission
+    CONSTRAINT FK_SurveyResponses_User FOREIGN KEY ([UserId]) REFERENCES [dbo].[User]([UserId]) ON DELETE CASCADE  -- Foreign key constraint
+);
+GO
+
